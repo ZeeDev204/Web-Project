0a3122e7857ccbbd9707ee044760def9cf3b8a8c
ZeeDev204 <zeeshanzakir11223344@gmail.com>
2025-09-01 21:53:51 +0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Project/',
+  base: '/Web-Project',
   server: {
     port: 5173,
     open: true
